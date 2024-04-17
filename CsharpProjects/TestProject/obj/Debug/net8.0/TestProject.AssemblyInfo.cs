643169a6841dbe4f4df702f04774ec0ac853b5c0
Roberto0611 <ochoacuevasr@gmail.com>
2024-04-17 11:25:44 -0700

    Greater number, method practice

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62863c02f45d80a240e688201e7365931c342527")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66ed1e2aac59487ee0f28521ae562d0167091cd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
