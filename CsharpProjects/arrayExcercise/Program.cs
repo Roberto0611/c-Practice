85dcf94b7098b5c2acf89cf063265591a47be026
Roberto0611 <ochoacuevasr@gmail.com>
2024-04-19 16:27:17 -0700

    Little foreach and arrays excercise

---
+++ CsharpProjects/arrayExcercise/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] keys = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+
+foreach(string key in keys){
+    if(key.StartsWith('B')){
+        Console.WriteLine(key);
+    }
+}
